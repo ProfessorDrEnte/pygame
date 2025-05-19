f61c7a4f1a4fdd3dc6c113ccd9c7d857fb23ca42
Prof. Dr. Ente <95550209+ProfessorDrEnte@users.noreply.github.com>
2025-05-19 21:54:32 +0200

    feat: Added shooting mechanic to player with cooldown

--- asteroid.py
+++ asteroid.py
@@ -10,3 +10,11 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+class Shot(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.velocity = pygame.Vector2(0, -300)  # Shot moves upwards
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "red", self.position, self.radius)  # Draw the shot in red
